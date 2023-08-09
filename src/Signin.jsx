87ed6b3f120cf403d333afed0fd8f68e69bea7c7
[SW5] 이주현 <leek71358@gmail.com>
2023-08-09 23:42:33 +0900

    로그인 코드 수정

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -4,9 +4,11 @@ import "./Signin.scss";
 import Header from "./Header";
 
 const Signin = () => {
+  const [isValid, setIsValid] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+
   useEffect(() => {
     const token = localStorage.getItem("access_token");
 
@@ -15,44 +17,28 @@ const Signin = () => {
     }
   }, []);
 
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  };
-  const handlePsd = (e) => {
-    setPassword(e.target.value);
-  };
-  // 이메일 정규식
-  const emailRegex = /\S+@\S+\.\S+/;
-
-  // 이메일 유효성 검사
-  const isEmailValid = emailRegex.test(email);
-  // 비밀번호 유효성 검사
-  const isPasswordValid = password.length >= 8;
+  useEffect(() => {
+    setIsValid(email.includes("@") && password.length > 7);
+  }, [email, password]);
 
   const onSubmit = (e) => {
     e.preventDefault();
 
-    if (!isEmailValid) {
-      alert("올바른 이메일 주소를 입력해주세요.");
-    } else if (!isPasswordValid) {
-      alert("비밀번호는 8자리 이상 입력해주세요.");
-    } else {
-      fetch("https://www.pre-onboarding-selection-task.shop/auth/signin", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-        }),
-      })
-        .then((res) => res.json())
-        .then((data) =>
-          data.access_token
-            ? (navigate("/todo"),
-              localStorage.setItem("access_token", data.access_token))
-            : console.log(data.message)
-        );
-    }
+    fetch("https://www.pre-onboarding-selection-task.shop/auth/signin", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) =>
+        data.access_token
+          ? (navigate("/todo"),
+            localStorage.setItem("access_token", data.access_token))
+          : console.log(data.message)
+      );
   };
   return (
     <>
@@ -63,18 +49,18 @@ const Signin = () => {
           <input
             data-testid="email-input"
             placeholder="이메일을 입력해주세요."
-            onChange={handleEmail}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
             data-testid="password-input"
             placeholder="비밀번호를 입력해주세요."
-            onChange={handlePsd}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <button
             data-testid="signin-button"
             onClick={onSubmit}
-            disabled={!isEmailValid || !isPasswordValid}
+            style={{ backgroundColor: isValid ? "green" : "red" }}
           >
             로그인
           </button>
