54b174d1b08b21d03ac275dde81916cb91309014
[SW5] 이주현 <leek71358@gmail.com>
2023-08-10 16:30:10 +0900

    할 일 추가,완료,수정,삭제

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,7 +1,14 @@
 import React, { useState, useCallback, useEffect } from "react";
 import "./TodoItem.scss";
 
-const TodoItem = ({ todos, setTodos, todo, handleTodoItemClick }) => {
+const TodoItem = ({
+  todos,
+  setTodos,
+  todo,
+  handleTodoItemClick,
+  BASE_URL,
+  getAuthHeaders,
+}) => {
   const [isUpdateClick, setIsUpdateClick] = useState(false);
   const [updateId, setUpdateId] = useState(null);
   const [updateValue, setUpdateValue] = useState("");
@@ -16,83 +23,99 @@ const TodoItem = ({ todos, setTodos, todo, handleTodoItemClick }) => {
   };
 
   const checkboxClick = useCallback(
-    (id) => {
-      setTodos((prevTodos) =>
-        prevTodos.map((todo) =>
-          todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
-        )
-      );
+    async (id) => {
+      const getTodoById = todos.find((todo) => todo.id === id);
 
-      const todoToUpdate = todos.find((todo) => todo.id === id);
-      if (todoToUpdate) {
-        fetch(`https://www.pre-onboarding-selection-task.shop/todos/${id}`, {
+      if (!getTodoById) return;
+
+      const updatedTodo = {
+        ...getTodoById,
+        isCompleted: !getTodoById.isCompleted,
+      };
+
+      try {
+        const response = await fetch(`${BASE_URL}/todos/${id}`, {
           method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-          },
-          body: JSON.stringify({
-            todo: todoToUpdate.todo,
-            isCompleted: !todoToUpdate.isCompleted,
-          }),
-        }).then((res) => res.json());
+          headers: getAuthHeaders(),
+          body: JSON.stringify(updatedTodo),
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to update todo");
+        }
+
+        setTodos((prevTodos) =>
+          prevTodos.map((todo) => (todo.id === id ? updatedTodo : todo))
+        );
+      } catch (error) {
+        console.error("Error updating todo:", error);
       }
     },
     [todos]
   );
 
-  const handleRemove = (id) => {
-    fetch(`https://www.pre-onboarding-selection-task.shop/todos/${id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-    }).then((res) => {
-      if (res.status === 204) {
-        setTodos((todos) => todos.filter((todo) => todo.id !== id));
-      } else {
-        console.error("삭제 오류:", res.status);
-      }
-    });
-  };
+  const handleRemove = useCallback(async (id) => {
+    try {
+      const response = await fetch(`${BASE_URL}/todos/${id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      });
 
-  const updateBtnClick = (id) => {
-    const todoToUpdate = todos.find((todo) => todo.id === id);
-    if (todoToUpdate) {
+      if (response.status === 204) {
+        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+      } else {
+        console.error("삭제 오류:", response.status);
+      }
+    } catch (error) {
+      console.error("API 요청 실패:", error);
+    }
+  });
+
+  const updateBtnClick = useCallback((id) => {
+    const getTodoById = todos.find((todo) => todo.id === id);
+    if (getTodoById) {
       setIsUpdateClick(true);
       setUpdateId(id);
     }
-  };
+  });
 
   const updateCancleClick = () => {
     setIsUpdateClick(false);
     setUpdateId(null);
   };
 
-  const updateSubmitClick = (id) => {
-    const todoToUpdate = todos.find((todo) => todo.id === id);
+  const updateSubmitClick = useCallback(async (id) => {
+    const getTodoById = todos.find((todo) => todo.id === id);
 
-    fetch(`https://www.pre-onboarding-selection-task.shop/todos/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-      body: JSON.stringify({
-        todo: updateValue,
-        isCompleted: todoToUpdate.isCompleted,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setTodos((prevTodos) =>
-          prevTodos.map((todo) =>
-            todo.id === id ? { ...todo, todo: updateValue } : todo
-          )
-        );
+    const updatedTodoData = {
+      todo: updateValue,
+      isCompleted: getTodoById.isCompleted,
+    };
+    try {
+      const response = await fetch(`${BASE_URL}/todos/${id}`, {
+        method: "PUT",
+        headers: getAuthHeaders(),
+        body: JSON.stringify(updatedTodoData),
       });
-    setIsUpdateClick(false);
-  };
+
+      if (!response.ok) {
+        throw new Error("Failed to update todo");
+      }
+
+      const data = await response.json();
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) =>
+          todo.id === id ? { ...todo, todo: updateValue } : todo
+        )
+      );
+
+      setIsUpdateClick(false);
+    } catch (error) {
+      console.error("Error updating todo:", error);
+    }
+  });
 
   return (
     <>
